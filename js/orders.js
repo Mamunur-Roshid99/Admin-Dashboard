3f744f971d4b4a409a823cb33847a04f00ed8f00
Mamunur Roshid <roshidmamunur569@gmail.com>
2024-08-18 18:38:29 +0600

    Add files via upload

---
+++ js/orders.js
@@ -0,0 +1,32 @@
+const Orders =[
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '56577',
+        productStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Larvender KF102 Drone',
+        productNumber: '3534',
+        productStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '897',
+        productStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '56803',
+        productStatus: 'paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Drone with camera Drone',
+        productNumber: '23456',
+        productStatus: 'paid',
+        shipping: 'Deliverd'
+    },
+]
